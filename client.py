25e30277a670db94f94fc2e4d21fcf78cb174454
Hakim <horairyhakim@gmail.com>
2025-12-19 17:10:03 +0100

    refactor: sent_text_to_sever function

--- client.py
+++ client.py
@@ -6,10 +6,10 @@ from config import HOST, PORT
 
 
 def send_text_to_server(text):
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as socket_object:
-        socket_object.connect((HOST, PORT))
-        socket_object.sendall(text.encode("utf-8"))
-        data = socket_object.recv(1024)
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
+        client_socket.connect((HOST, PORT))
+        client_socket.sendall(text.encode("utf-8"))
+        data = client_socket.recv(1024)
 
     print("Réponse du serveur :", data.decode())
 
